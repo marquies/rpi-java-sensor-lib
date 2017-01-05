2c034a5c00a28085d893cb820891867259e9abb0
Patrick Steinert <patrick.breucking@t-systems.com>
2017-01-06 00:03:13 +0100

    Refactoring

---
+++ examples/src/main/java/de/patricksteinert/rpisensorlib/examples/Tsl45315Example.java
@@ -0,0 +1,13 @@
+package de.patricksteinert.rpisensorlib.examples;
+
+import de.patricksteinert.rpisensorlib.TSL45315;
+
+/**
+ * Created by Patrick Steinert on 05.01.17.
+ */
+public class Tsl45315Example {
+
+    public static void main(String[] args) {
+        TSL45315 tsl45315 = new TSL45315();
+    }
+}
