84a5def1c8ea6c04ad0f68eac6a58cb2b272a1db
Patrick Steinert <patrick.breucking@t-systems.com>
2017-01-06 07:28:37 +0100

    Completed first TSL4531 Example

--- examples/src/main/java/de/patricksteinert/rpisensorlib/examples/Tsl45315Example.java
+++ examples/src/main/java/de/patricksteinert/rpisensorlib/examples/Tsl45315Example.java
@@ -2,12 +2,16 @@ package de.patricksteinert.rpisensorlib.examples;
 
 import de.patricksteinert.rpisensorlib.TSL45315;
 
+import java.io.IOException;
+
 /**
  * Created by Patrick Steinert on 05.01.17.
  */
 public class Tsl45315Example {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TSL45315 tsl45315 = new TSL45315();
+        double value = tsl45315.readValue();
+        System.out.println("TSL45315 (light) Sensor read '" + value + "' Lux");
     }
 }
